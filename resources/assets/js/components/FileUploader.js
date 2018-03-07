75cfb2a527fb508e08785ada3def70d97b0838e2
error039 <ruanzihang1@gmail.com>
2018-03-07 14:54:27 -0800

    Frontend upload file

--- resources/assets/js/components/FileUploader.js
+++ resources/assets/js/components/FileUploader.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
+import axios, { post } from 'axios';
 
 export default class FileUploader extends Component {
     constructor(props) {
       super(props);
-      this.state = {video: null};
+      this.state = {
+        video: null,
+        status: 0, // 1 is success, 2 is fail
+        error: '',
+      };
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.fileUpload = this.fileUpload.bind(this);
+      this.fileChange = this.fileChange.bind(this);
     }
 
     handleSubmit(e) {
       e.preventDefault();
       console.log(this.state.video);
+      this.fileUpload(this.state.video).then((response) => {
+          console.log(response.data);
+          this.setState({ status: 1 });
+      })
+      .catch( (error) => {
+        console.log(error);
+        this.setState({ status: 2, error: error });
+      });
+    }
+
+    fileUpload(file) {
+      const url = 'http://localhost:8000/api/uploadVideo';
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('token', this.props.token);
+      const config = {
+          headers: {
+              'content-type': 'multipart/form-data'
+          }
+      }
+      return post(url, formData, config)
     }
 
     fileChange (e) {
@@ -29,13 +57,13 @@ export default class FileUploader extends Component {
                             <div className="card-header">Upload Your Video</div>
 
                             <div className="card-body">
-                                <form onSubmit={this.handleSubmit.bind(this)}>
+                                <form onSubmit={this.handleSubmit}>
                                   <input
                                     name="video"
                                     type="file"
                                     accept=".mp4"
-                                    form-control-file
-                                    onChange={this.fileChange.bind(this)}
+                                    onChange={this.fileChange}
+                                    required
                                   />
                                   <button
                                     type="submit"
@@ -46,6 +74,18 @@ export default class FileUploader extends Component {
                                 </form>
                             </div>
                         </div>
+                        <div
+                          className="alert alert-success mt-2"
+                          hidden={this.state.status !== 1}
+                        >
+                          <strong>Success!</strong> Video uploaded
+                        </div>
+                        <div
+                          className="alert alert-danger mt-2"
+                          hidden={this.state.status !== 2}
+                        >
+                          <strong>Error!</strong> {this.state.error}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -54,5 +94,7 @@ export default class FileUploader extends Component {
 }
 
 if (document.getElementById('FileUploader')) {
-    ReactDOM.render(<FileUploader />, document.getElementById('FileUploader'));
+    const element = document.getElementById('FileUploader');
+    const props = Object.assign({}, element.dataset);
+    ReactDOM.render(<FileUploader {...props} />, element);
 }
