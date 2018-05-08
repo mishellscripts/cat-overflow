d3477ab6295c6f101c6d18a155434193e7ecec37
Michelle Song <michelle.song@sjsu.edu>
2018-05-08 13:50:19 -0700

    Add side by side video

--- resources/views/video/single.blade.php
+++ resources/views/video/single.blade.php
@@ -3,15 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ $video->name }}</div>
 
                 <div class="card-body">
-                    <video width="100%" height="auto" style="background-color:#000" controls> 
+
+                    <video width="50%" height="auto" style="background-color:#000; float:left;" controls autoplay>
+                        <source src="{{ asset("storage/original_videos/$video->id.mp4") }}" type="video/mp4">
+                        Your browser does not support the video.
+                    </video>
+
+                    <video width="50%" height="auto" style="background-color:#000; float:left;" controls autoplay>
                         <source src="{{ asset("storage/processed_videos/$video->id.mp4") }}" type="video/mp4">
                         Your browser does not support the video.
                     </video>
+
                 </div>
             </div>
         </div>
