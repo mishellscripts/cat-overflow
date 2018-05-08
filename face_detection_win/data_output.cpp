e647b78e4c4714c0eaab91081b188843f308861b
RUI LI <lr_280@qq.com>
2018-05-07 17:27:28 -0700

    change json output

--- face_detection_win/data_output.cpp
+++ face_detection_win/data_output.cpp
@@ -8,12 +8,8 @@ using namespace std;
 using namespace dlib;
 
 void output_face_landmarks(std::vector<full_object_detection> shapes,
-                           std::vector<Dual_Points> &eyes,
-                           cv::Vec3d ypr,
                            nlohmann::json &json)
 {
-    //creates Json object
-    //cout << "debug out 23" << endl;
     nlohmann::json json_object;
     for(unsigned int i = 0; i < shapes.size(); i++) {
         //Adds all (x,y) points to json object
@@ -24,6 +20,21 @@ void output_face_landmarks(std::vector<full_object_detection> shapes,
             json_object[face][point]["x_coord"] = shapes[i].part(j).x();
             json_object[face][point]["y_coord"] = shapes[i].part(j).y();
         }
+    }
+    json = json_object;
+}
+// .png-face.json
+// .png-eyes-ypr.json
+
+void output_eyes_ypr(std::vector<Dual_Points> &eyes,
+                     cv::Vec3d ypr,
+                     nlohmann::json &json)
+{
+    nlohmann::json json_object;
+    for(unsigned int i = 0; i < eyes.size(); i++) {
+        //Adds all (x,y) points to json object
+        string face = "face_" + to_string(i);
+
         json_object[face]["left_eye"]["x_coord"] = eyes[i].point1.x;
         json_object[face]["left_eye"]["y_coord"] = eyes[i].point1.y;
 
@@ -34,17 +45,5 @@ void output_face_landmarks(std::vector<full_object_detection> shapes,
         json_object[face]["pitch"] = ypr[0];
         json_object[face]["roll"] = ypr[2];
     }
-    //cout << "debug out 42" << endl;
-    //creates json file and dumps json object
-    /*
-    std::ofstream json_file;
-    json_file.open(file_name + ".json");
-    if (json_file.is_open())
-    {
-        //cout << "debug out 48" << endl;
-        json_file << json_object.dump(4) << std::endl;
-        json_file.close();
-    }
-     */
     json = json_object;
 }
